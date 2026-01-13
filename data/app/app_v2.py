026d6912ffb3b060a4003d4e33129ad58351188a
Bruno Ricardo Machado <bruno.rm@grad.ufsc.br>
2026-01-13 16:46:33 -0300

    fix: corrigindo chamada do app no uvicorn

--- data/app/app_v2.py
+++ data/app/app_v2.py
@@ -87,4 +87,4 @@ def predict(request: SentimentRequest):
         return {"erro": "Modelo Multi não carregado no servidor"}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("app_v2:app", host="0.0.0.0", port=8000)
